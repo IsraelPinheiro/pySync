b389615f1a70605976ae3fd6b5ba83da07985539
ricassiocosta <riksiocosta@gmail.com>
2020-10-29 18:28:42 -0300

    code refactoring

--- client.py
+++ client.py
@@ -1,13 +1,20 @@
 import os, time
-path_to_watch = "."
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-while True:
-  time.sleep (5)
-  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-  file_added = [f for f in after if not f in before]
-  file_removed = [f for f in before if not f in after]
-  if file_added: 
-    print ("file_added: ", ", ".join (file_added))
-  if file_removed: 
-    print ("file_removed: ", ", ".join (file_removed))
-  before = after
+
+def update(status, file):
+  print("status: ", status, "file: ", file)
+
+def watch_dir(path_to_watch):
+  before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+  while True:
+    after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+    file_added = [f for f in after if not f in before]
+    file_removed = [f for f in before if not f in after]
+    if file_added: 
+      update("Create", file_added)
+    if file_removed: 
+      update("Delete", file_removed)
+    before = after
+
+if __name__ == "__main__":
+  path_to_watch = "."
+  watch_dir(path_to_watch)
